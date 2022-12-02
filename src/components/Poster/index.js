d9666b4a63c054ddb2ecaa2a7b2b4d08ed16b499
Cahya Ramadhan <cahyaramadhan76@gmail.com>
2022-12-02 16:12:39 +0700

    feat: add download poster function

--- src/components/Poster/index.js
+++ src/components/Poster/index.js
@@ -1,5 +1,12 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import { useState, useEffect, useRef } from "react";
 import { PosterService } from "../../services/PosterService";
+
+import { ImSpinner2 } from "react-icons/im";
 
 import Modal from "../Modal";
 
@@ -71,14 +78,19 @@ const ModalOpenImage = ({ show, setShow, openedPoster }) => {
 };
 
 const Poster = (props) => {
+  const [loadingGetPoster, setLoadingGetPoster] = useState(false);
+  const [loadingDownloadPoster, setLoadingDownloadPoster] = useState(false);
+
   const [posters, setPosters] = useState([]);
 
   useEffect(() => {
     const getPoster = async () => {
+      setLoadingGetPoster(true);
       const result = await PosterService.getPosters();
       if (Array.isArray(result?.data)) {
         setPosters(result?.data);
       }
+      setLoadingGetPoster(false);
     };
     getPoster();
   }, []);
@@ -86,35 +98,135 @@ const Poster = (props) => {
   const [showModalPoster, setShowModalPoster] = useState(false);
   const [openedPoster, setOpenedPoster] = useState("");
 
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const toastId = "download-poster-result";
+  const messageSuccess = (message) =>
+    toast.success(message, {
+      toastId: toastId,
+    });
+
+  const messageFailed = (message) =>
+    toast.error(message, {
+      toastId: toastId,
+    });
+
+  const downloadPoster = async () => {
+    if (startDate && endDate) {
+      setLoadingDownloadPoster(true);
+      const result = await PosterService.downloadPoster(startDate, endDate);
+      if (result?.data === "download poster success") {
+        messageSuccess(result?.data);
+      } else {
+        messageFailed(result?.data || result?.code);
+      }
+      setLoadingDownloadPoster(false);
+    }
+  };
+
   return (
     <>
-      <div className="flex flex-col gap-4 px-6 py-20">
-        <div className="flex flex-row gap-4">
-          <div className="btn">Download Poster</div>
+      <div className="flex flex-col gap-8 px-6 py-16">
+        <div className="flex flex-row gap-4 items-end">
+          <div className="w-full flex flex-col gap-2">
+            <label className="text-lg font-bold text-[#000000]">
+              Start date
+            </label>
+            <div
+              className="flex flex-row items-center rounded-lg px-3 min-h-[48px] 
+              w-full text-lg input border-opacity-10 border-[#472F92] focus:outline-none
+              border-2 border-solid gap-3"
+            >
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => {
+                  setStartDate(date);
+                  if (endDate < date) {
+                    setEndDate(date);
+                  }
+                }}
+                className="border-none focus:outline-none w-full"
+                placeholderText="dd-mm-yyyy"
+                dateFormat="dd-MMM-yyyy"
+              />
+            </div>
+          </div>
+          <div className="w-full flex flex-col gap-2">
+            <label className="text-lg font-bold text-[#000000]">End date</label>
+            <div
+              className="flex flex-row items-center rounded-lg px-3 min-h-[48px] 
+              w-full text-lg input border-opacity-10 border-[#472F92] focus:outline-none
+              border-2 border-solid gap-3"
+            >
+              <DatePicker
+                selected={endDate}
+                minDate={startDate}
+                onChange={(date) => setEndDate(date)}
+                className="border-none focus:outline-none w-full"
+                placeholderText="dd-mm-yyyy"
+                dateFormat="dd-MMM-yyyy"
+              />
+            </div>
+          </div>
+          <div
+            className={`${
+              loadingDownloadPoster ? "btn-disabled" : ""
+            } btn w-[11rem] min-w-[11rem]`}
+            onClick={() => downloadPoster()}
+          >
+            {loadingDownloadPoster ? (
+              <ImSpinner2 className="animate-spin" />
+            ) : (
+              "Download Poster"
+            )}
+          </div>
         </div>
         <div
-          className="grid grid-cols-4 gap-4 p-6 border-2 border-solid border-gray-500 
-        justify-items-center items-center"
+          className={`${
+            loadingGetPoster || posters?.length > 0
+              ? "grid grid-cols-4 gap-4"
+              : ""
+          } p-6 border-2 border-solid border-gray-500 
+          justify-items-center items-center`}
         >
-          {posters?.map((poster, idx) => (
-            <div className="flex flex-col w-full gap-2 p-1 bg-gray-200">
-              <img
+          {loadingGetPoster ? (
+            <>
+              <div className="w-[24rem] h-[24rem] bg-gray-300 animate-pulse"></div>
+              <div className="w-[24rem] h-[24rem] bg-gray-300 animate-pulse"></div>
+              <div className="w-[24rem] h-[24rem] bg-gray-300 animate-pulse"></div>
+              <div className="w-[24rem] h-[24rem] bg-gray-300 animate-pulse"></div>
+              <div className="w-[24rem] h-[24rem] bg-gray-300 animate-pulse"></div>
+              <div className="w-[24rem] h-[24rem] bg-gray-300 animate-pulse"></div>
+              <div className="w-[24rem] h-[24rem] bg-gray-300 animate-pulse"></div>
+            </>
+          ) : posters?.length > 0 ? (
+            posters?.map((poster, idx) => (
+              <div
                 key={idx}
-                src={poster?.url}
-                alt={poster?.filename}
-                className="w-full max-h-[24rem] cursor-pointer"
-                onClick={() => {
-                  setOpenedPoster(poster);
-                  setShowModalPoster(true);
-                }}
-              />
-              <span className="w-full text-center pt-1 pb-2 font-bold">
-                {poster?.filename}
-              </span>
-            </div>
-          ))}
+                className="flex flex-col w-full gap-2 p-1 bg-gray-200"
+              >
+                <img
+                  key={idx}
+                  src={poster?.url}
+                  alt={poster?.filename}
+                  className="w-full max-h-[24rem] cursor-pointer"
+                  onClick={() => {
+                    setOpenedPoster(poster);
+                    setShowModalPoster(true);
+                  }}
+                />
+                <span className="w-full text-center pt-1 pb-2 font-bold">
+                  {poster?.filename}
+                </span>
+              </div>
+            ))
+          ) : (
+            <div className="w-full text-center font-bold">No data</div>
+          )}
         </div>
       </div>
+      <ToastContainer />
       <ModalOpenImage
         show={showModalPoster}
         setShow={setShowModalPoster}
--- src/services/PosterService.js
+++ src/services/PosterService.js
@@ -2,6 +2,7 @@ import axiosInstance from "./AxiosInstance";
 
 export const PosterService = {
   getPosters,
+  downloadPoster,
 };
 
 async function getPosters() {
@@ -9,6 +10,25 @@ async function getPosters() {
     .get("/poster")
     .then((res) => res)
     .catch((e) => console.log(e));
+
+  return result;
+}
+
+async function downloadPoster(startDate, endDate) {
+  const result = await axiosInstance
+    .post(
+      "/download-poster",
+      {
+        startDate: startDate,
+        endDate: endDate,
+      },
+      {}
+    )
+    .then((res) => res)
+    .catch((e) => {
+      console.log(e);
+      return e;
+    });
 
   return result;
 }
